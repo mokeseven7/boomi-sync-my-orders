e0a7c70b61679568f6b92b22831813505da37121
Mike McGrath <mike.mcgrath@rallysportdirect.com>
2020-01-04 15:12:16 -0700

    accept single record or batch of records

--- function/boomiSyncMyOrders.php
+++ function/boomiSyncMyOrders.php
@@ -7,18 +7,39 @@ use Popcorn\Beans\Models\QueueTask;
 
 function boomiSyncMyOrders($data)
 {
-	$task = new QueueTask(['host' => '34.210.227.78', 'database' => 'rally-local']);
+	//Initialize a mongo connection
+	$queueTask = new QueueTask(['host' => '34.210.227.78', 'database' => 'rally-local']);
 
-	$record = QueueTask::make(
-		$data['task'],
-		$data['parameters'],
-		[
-			'batch' => $data['batch'],
-			'sales_channel' => $data['sales_channel']
-		]
-	);
+	//If the request contains a batch of records, loop through them
+	if (!isset($data['task'])) {
+		$tasks = [];
+		foreach ($data as $task) {
+			//Make a mongo task record for each object in request
+			$tasks[] = QueueTask::make(
+				$task['task'],
+				$task['parameters'],
+				[
+					'batch' => $task['batch'],
+					'sales_channel' => $task['sales_channel']
+				]
+			);
+		}
+	} else {
+		//Only one record sent, make a mongo task 
+		$tasks = QueueTask::make(
+			$data['task'],
+			$data['parameters'],
+			[
+				'batch' => $data['batch'],
+				'sales_channel' => $data['sales_channel']
+			]
+		);
+	}
 
-	$result = $task->insertOne($record);
+	print_r($tasks);
+
+	//insert transformed task(s)
+	$result = $queueTask->insertOne($tasks);
 
 	$response = [
 		'status' => 'ok',
